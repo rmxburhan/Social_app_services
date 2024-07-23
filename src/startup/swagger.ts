371d977edc18a78e8c5bd1969b326e5b1cefb49b
Rizal Burhanudin <rizalburhanudin556@gmail.com>
2024-07-23 13:02:13 +0700

    remove swagger

--- src/startup/swagger.ts
+++ src/startup/swagger.ts
@@ -1,16 +1,16 @@
-import { Express } from "express";
-import swaggerUi from "swagger-ui-express";
-const swaggerDocument = require("../../docs/swagger/swagger.json");
+// import { Express } from "express";
+// import swaggerUi from "swagger-ui-express";
+// const swaggerDocument = require("../../docs/swagger/swagger.json");
 
-var options = {
-  explorer: true,
-};
-const initSwagger = (app: Express) => {
-  swaggerDocument.host = process.env.APP_HOST || "http://localhost:5000";
-  app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument, options)
-  );
-};
-export default initSwagger;
+// var options = {
+//   explorer: true,
+// };
+// const initSwagger = (app: Express) => {
+//   swaggerDocument.host = process.env.APP_HOST || "http://localhost:5000";
+//   app.use(
+//     "/api-docs",
+//     swaggerUi.serve,
+//     swaggerUi.setup(swaggerDocument, options)
+//   );
+// };
+// export default initSwagger;
